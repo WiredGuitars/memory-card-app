4b303ad8885daeac95b09942c4171e324298c83d
Daniel Laschanzky <dlaschanzky2@huskers.unl.edu>
2023-10-08 19:04:50 -0500

    RenderCards component created, fetched data from Pokeapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Cards from './Components/RenderCards'
 
-function App() {
-
+export default function App() {
+  return (
+    <>
+    <Cards />
+    </>      
+  )
 }
-
-export default App
